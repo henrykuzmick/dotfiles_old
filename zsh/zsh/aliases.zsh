fdb1941f1538c5a6ff7e242a4386acf01f30552a
Henrikas Kuzmickas <henry.kuzmick@gmail.com>
2017-07-05 14:02:59 +0200

    Initial commit

---
+++ zsh/zsh/aliases.zsh
@@ -0,0 +1,38 @@
+# ZSH
+alias reload!='. ~/.zshrc'
+
+# GIT
+alias ga="git add"
+alias gc="git commit"
+alias gph='git push'
+alias gpl='git pull --rebase'
+alias gd='git diff --compaction-heuristic'
+alias gco='git checkout'
+alias gs='git status -sb'
+alias gcp='git cherry-pick'
+alias gcl='git clone'
+alias gg='git grep'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+alias glc="git log --graph --color --date=relative --stat"
+
+# BUNDLER
+alias b='bundle'
+alias be='b exec'
+alias bea='be autotest -c'
+alias brake='bin/rake'
+alias brspec='bin/rspec'
+alias brails='bin/rails'
+
+# ATOM
+alias a='atom .'
+
+# CORDOVA
+alias cb='cordova build'
+alias cr='cordova run'
+
+# MAC OS X
+alias lock="/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine"
+alias out='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
+
+# LINES OF CODE
+alias lr='find . -name "*.rb" -print | xargs wc -l | tail -n 1'
---
+++ zsh/zsh/paths.zsh
@@ -0,0 +1,11 @@
+# Own bins
+export PATH="$PATH:/Users/henrikas/Documents/dotfiles/bin"
+
+# Rails bins from Quipu
+export PATH="$PATH:/Users/henrikas/Documents/quipuapp/bin"
+
+# MySQL path
+export PATH="$PATH:/usr/local/mysql/bin"
+
+# Fix RVM issue
+export rvmsudo_secure_path=1
